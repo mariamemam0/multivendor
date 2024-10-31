fdfb58d6030ade3ca6253da3c6cd6482ddd8ecfe
mariam <mariamemampp00@gmail.com>
2024-10-31 11:43:45 +0300

    make routes to contorl the trashed categories

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -12,7 +12,13 @@ Route::group([
     Route::get('/', [DashboardController::class, 'index'])
         
         ->name('dashboard');
-
+        Route::get('/categories/trash',[CategoriesController::class,'trash'])
+        ->name('categories.trash');
+        Route::put('/categories/{category}/restor',[CategoriesController::class,'restore'])
+        ->name( 'categories.restore');
+        Route::delete('/categories/{category}/force-delete',[CategoriesController::class,'forceDelete'])
+        ->name( 'categories.force-delete');
+   
     Route::resource('/categories', CategoriesController::class);
        
 
