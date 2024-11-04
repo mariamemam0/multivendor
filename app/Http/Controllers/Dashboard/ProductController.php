40db6fd3d238eedcc5707b82965df43f648c44e6
mariam <mariamemampp00@gmail.com>
2024-11-04 11:49:06 +0200

    make global scope store to apply it on all product model

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -14,14 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $user = Auth::user();
-        if($user->store_id){
-              $products = Product::where('store_id', '=', $user->store_id)->paginate();
-        }
-        else
-        {
-            $products = Product::paginate();
-         }
+        $products = Product::paginate();
          return view('dashboard.products.index',compact('products'));
 
     }
