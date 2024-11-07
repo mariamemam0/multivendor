10e5ff7807ec0e6ebbd1fa5dc88da8ff30fbba5d
mariam <mariamemampp00@gmail.com>
2024-11-07 14:42:39 +0200

    use the relations to make relationship between the category table and itself to show the name of parent instead of using the join

--- app/Http/Controllers/Dashboard/CategoriesController.php
+++ app/Http/Controllers/Dashboard/CategoriesController.php
@@ -21,11 +21,12 @@ class CategoriesController extends Controller
         $request =request();
          
 
-        $categories= Category::leftJoin('categories as parents','parents.id','=','categories.parent_id')
+        $categories= Category::with('parent')/*leftJoin('categories as parents','parents.id','=','categories.parent_id')
         ->select([
             'categories.*',
             'parents.name as parent_name'
-        ])
+        ])*/
+        
         ->filter($request->query())
         ->orderBy('categories.name')
         ->paginate();
