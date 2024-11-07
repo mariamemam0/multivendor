7b0417671531299e63ae70516dbc0b60678d0e44
mariam <mariamemampp00@gmail.com>
2024-11-07 14:23:25 +0200

    make relations between the tables

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,7 +15,11 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::paginate();
+        $products = Product::with(['category','store'])->paginate();
+        //select * from products
+        //select * from categories where id in (...value that will be passed by laravel ) 
+        //select * from stores where id in (...value that will be passed by laravel) 
+         
          return view('dashboard.products.index',compact('products'));
 
     }
