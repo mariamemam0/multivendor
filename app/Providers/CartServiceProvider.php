7ff95da7a53e7097adbadae9bd67ceec4ddafed8
mariam <mariamemampp00@gmail.com>
2024-11-23 13:12:06 +0200

    make repositries to deal with it

---
+++ app/Providers/CartServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+use App\Repositeries\Cart\CartModelRepository;
+use App;
+use App\Repositeries\Cart\CartRepository;
+use Illuminate\Support\ServiceProvider;
+
+class CartServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(CartRepository::class,function() {
+            return new CartModelRepository();
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
--- app/Repositeries/Cart/CartRepository.php
+++ app/Repositeries/Cart/CartRepository.php
@@ -1,2 +1,18 @@
 <?php
 namespace App\Repositeries\Cart;
+
+use App\Models\Product;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cookie;
+
+interface CartRepository
+{
+    public function get() : Collection;
+   public function add(Product $product ,$quantity = 1);
+   public function update(Product $product);
+   Public function delete($id);
+   public function empty();
+   public function total():float;
+   
+   
+}
