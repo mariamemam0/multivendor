5c230c7070a1164a06de3117b967bfadb0e0d469
mariam <mariamemampp00@gmail.com>
2024-12-14 14:08:28 +0200

    cancle breeze pakcage to use fortify for authentication

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,5 @@ Route::resource('cart' , CartController::class);
 Route::get('checkout',[CheckoutController::class,'create'])->name(name: 'checkout');
 Route::post('checkout',[CheckoutController::class,'store']);
 
-require __DIR__.'/auth.php';
+//require __DIR__.'/auth.php';
 require __DIR__.'/dashboard.php';
