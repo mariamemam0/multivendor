f740677e0540209096bb1592a9660a10a8d0ee5f
mariam <mariamemampp00@gmail.com>
2024-12-17 16:07:44 +0200

    add route to use two-factor-authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Front\Auth\TwoFactorAuthenticationController;
 use App\Http\Controllers\Front\CartController;
 use App\Http\Controllers\Front\CheckoutController;
 use App\Http\Controllers\Front\HomeController;
@@ -27,6 +28,7 @@ Route::get('/products/{product:slug}',[ProductsController::class,'show'])->name(
 Route::resource('cart' , CartController::class);
 Route::get('checkout',[CheckoutController::class,'create'])->name(name: 'checkout');
 Route::post('checkout',[CheckoutController::class,'store']);
-
+Route::get('auth/user/2fa',[TwoFactorAuthenticationController::class,'index'])
+->name('front.2fa');
 //require __DIR__.'/auth.php';
 require __DIR__.'/dashboard.php';
