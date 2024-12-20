3b7deed98ed0e6f2b151a80f62fcfbe59e715e12
mariam <mariamemampp00@gmail.com>
2024-12-20 16:04:29 +0200

    change the appearence of the data in the responce

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -16,6 +16,23 @@ class ProductResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'name'=> $this->name,
+            'price'=> [
+                'normal'=>$this->price,
+                'compare'=>$this->compare_price
+            ],
+            'description'=> $this->description,
+            'image'=>$this->image_url,
+            'relations'=>[
+            'category'=>[
+                'id' =>$this->category->id,
+                'name' =>$this->category->name,
+            ],
+            'store'=>[
+                'id'=> $this->store->id,
+                'name'=> $this->store->name,
+            ],
+        ],
         ];
     }
 }
