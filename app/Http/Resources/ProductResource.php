d65db2110020a6aac896c0d8516ec98a96f155c8
mariam <mariamemampp00@gmail.com>
2024-12-20 14:53:12 +0200

    $this : refer to product model

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -15,7 +15,7 @@ class ProductResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-           
+            'id' => $this->id,
         ];
     }
 }
