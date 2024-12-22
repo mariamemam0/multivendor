8ca78c7b260b96a0b126601f1d49145f081f9fb9
mariam <mariamemampp00@gmail.com>
2024-12-22 09:31:16 +0200

    make action to delete the token

--- app/Http/Controllers/Api/AccessTokensController.php
+++ app/Http/Controllers/Api/AccessTokensController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Response;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class AccessTokensController extends Controller
 {
@@ -36,4 +38,19 @@ class AccessTokensController extends Controller
         'message'=>'Invalid Credentials',
     ],401);
 }
+
+   public function destroy($token = null)
+   {
+          $user = Auth::guard('sanctum')->user();
+          if(null === $token){
+            $user->currentAccessToken()->delete();
+            return;
+          }
+           $personalAccessToken =  PersonalAccessToken::findToken($token);
+           if($user->id == $personalAccessToken->tokenable_id 
+           && get_class($user) == $personalAccessToken->tokenable_type){
+            $personalAccessToken->delete();
+           }
+           
+   }
 }
