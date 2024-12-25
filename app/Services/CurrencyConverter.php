a2c1121baadc14de5fcec58c52cc49177994dcfd
mariam <mariamemampp00@gmail.com>
2024-12-25 11:56:13 +0200

    add class to handle the service

--- app/Services/CurrencyConverter.php
+++ app/Services/CurrencyConverter.php
@@ -1,27 +1,33 @@
 <?php
+
 namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
+
 class CurrencyConverter
 {
+    private $apiKey;
+
     protected $baseUrl = 'https://free.currconv.com/api/v8';
-   private $apiKey;
-   public function __construct(string $apiKey)
-   {
-    $this->apiKey = $apiKey;
-   } 
-   public function convert(  string $from, string $to, float $amount = 1): float
-   {
-    $q = "{$from}_{$to}";
+
+    public function __construct(string $apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    public function convert(string $from, string $to, float $amount = 1): float
+    {
+        $q = "{$from}_{$to}";
         $response = Http::baseUrl($this->baseUrl)
-       ->get('/convert', [
-           'q' => $q,
-           'compact' => 'y',
-           'apiKey' => $this->apiKey,
-           ]);
-              $result = $response->json();
-             return $result[$q] * $amount;
-   }
+            ->get('/convert', [
+                'q' => $q,
+                'compact' => 'y',
+                'apiKey' => $this->apiKey,
+            ]);
 
-
+        $result = $response->json();
+        //dd($result);
+        
+        return $result[$q]['val'] * $amount;
+    }
 }
