b20ada44cce848bb0c3dcddfc492c68b4a408143
mariam <mariamemampp00@gmail.com>
2024-12-26 16:20:42 +0200

    make inline translation calls

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -87,9 +87,9 @@
                     <div class="col-lg-4 col-md-4 col-12">
                         <div class="top-middle">
                             <ul class="useful-links">
-                                <li><a href="index.html">Home</a></li>
-                                <li><a href="about-us.html">About Us</a></li>
-                                <li><a href="contact.html">Contact Us</a></li>
+                                <li><a href="index.html">{{trans('app.home')}}</a></li>
+                                <li><a href="about-us.html">@lang('app.about')</a></li>
+                                <li><a href="contact.html">{{__('app.contact')}}</a></li>
                             </ul>
                         </div>
                     </div>
@@ -111,14 +111,14 @@
                             @else
                             <div class="user">
                                 <i class="lni lni-user"></i>
-                                Hello
+                                {{__('app.hello')}}
                             </div>
                             <ul class="user-login">
                                 <li>
-                                    <a href="{{ route('login') }}">{{ Lang::get('Sign In') }}</a>
+                                    <a href="{{ route('login') }}">{{ Lang::get('app.signin') }}</a>
                                 </li>
                                 <li>
-                                    <a href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a href="{{ route('register') }}">{{ __('app.register') }}</a>
                                 </li>
                             </ul>
                             @endauth
