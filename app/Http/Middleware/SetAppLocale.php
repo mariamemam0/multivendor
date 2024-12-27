89085bb7dcc91e815d8afe09446eedc8e5299936
mariam <mariamemampp00@gmail.com>
2024-12-27 10:06:13 +0200

    get lang from route instead of cookie

--- app/Http/Middleware/SetAppLocale.php
+++ app/Http/Middleware/SetAppLocale.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 use Symfony\Component\HttpFoundation\Response;
 
 class SetAppLocale
@@ -18,9 +20,15 @@ class SetAppLocale
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $locale = request('locale',Cookie::get('locale',config('app.locale'))); 
+       // $locale = request('locale',Cookie::get('locale',config('app.locale'))); 
+        //Cookie::queue('locale',$locale,60*24*365);
+
+       $locale = $request->route('locale');
           App::setLocale($locale);
-          Cookie::queue('locale',$locale,60*24*365);
+          URL::defaults([
+            'locale' =>$locale,
+          ]);
+          Route::current()->forgetParameter('locale');
         return $next($request);
     }
 }
