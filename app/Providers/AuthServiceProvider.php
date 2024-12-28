b61e65f2e053878efe9332232cfb35ac66ca021b
mariam <mariamemampp00@gmail.com>
2024-12-28 21:20:12 +0200

    define autherization gates to user it in dashboard

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,22 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('categories.view',function($user){
+             return true;
+        });
+
+        Gate::define('categories.create',function($user){
+            return true;
+
+        });
+
+        Gate::define('categories.update',function($user){
+            return false;
+
+        });
+        Gate::define('categories.delete',function($user){
+            return false;
+
+        });
     }
 }
