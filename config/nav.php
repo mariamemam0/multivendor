7a6e691cee9cc32c5c29a6a4b290886493233180
mariam <mariamemampp00@gmail.com>
2024-12-29 21:40:57 +0200

    add abilities to nav

--- config/nav.php
+++ config/nav.php
@@ -11,18 +11,23 @@ return [
      'title'=> 'Categories'  ,
      'badge'=>'New',  
     'active'=>'dashboard.categories.*',
+    'ability'=>'categories.view',
     ],
     ['icon'=>'far fa-circle nav-icon',
      'route'=> 'dashboard.products.index',
      'title'=> 'Products' ,
      'active'=>'dashboard.products.*',
+     'ability'=>'products.view',
+
      
 
     ],
-    ['icon'=>'far fa-circle nav-icon',
-     'route'=> 'dashboard.products.index',
-     'title'=> 'Orders' ,
+    ['icon'=>'far fa-shield nav-icon',
+     'route'=> 'dashboard.roles.index',
+     'title'=> 'Roles' ,
      'active'=>'dashboard.orders.*',
+     //'ability'=>'orders.view',
+
    
     ]
 
