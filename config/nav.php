3a3e2768e64e2b81485328e59dd99a84d823ce97
mariam <mariamemampp00@gmail.com>
2025-01-02 18:55:35 +0200

    improve nav and add admin and users

--- config/nav.php
+++ config/nav.php
@@ -1,43 +1,53 @@
-<?php 
+<?php
 
 return [
-    ['icon'=>'nav-icon fas fa-tachometer-alt',
-     'route'=> 'dashboard.dashboard',
-     'title'=> 'Dashboard'  ,
-    'active'=>'dashboard.dashboard',
+    [
+        'icon' => 'nav-icon fas fa-tachometer-alt',
+        'route' => 'dashboard.dashboard',
+        'title' => 'Dashboard',
+        'active' => 'dashboard.dashboard',
     ],
-    ['icon'=>'far fa-circle nav-icon',
-     'route'=> 'dashboard.categories.index',
-     'title'=> 'Categories'  ,
-     'badge'=>'New',  
-    'active'=>'dashboard.categories.*',
-    'ability'=>'categories.view',
+    [
+        'icon' => 'fas fa-tags nav-icon',
+        'route' => 'dashboard.categories.index',
+        'title' => 'Categories',
+        'badge' => 'New',
+        'active' => 'dashboard.categories.*',
+        'ability' => 'categories.view',
     ],
-    ['icon'=>'far fa-circle nav-icon',
-     'route'=> 'dashboard.products.index',
-     'title'=> 'Products' ,
-     'active'=>'dashboard.products.*',
-     'ability'=>'products.view',
-
-     
-
+    [
+        'icon' => 'fas fa-box nav-icon',
+        'route' => 'dashboard.products.index',
+        'title' => 'Products',
+        'active' => 'dashboard.products.*',
+        'ability' => 'products.view',
     ],
-    ['icon'=>'far fa-receipt nav-icon',
-     'route'=> 'dashboard.categories.index',
-     'title'=> 'Orders' ,
-     'active'=>'dashboard.orders.*',
-     'ability'=>'orders.view',
-
-     
-
+    [
+        'icon' => 'fas fa-receipt nav-icon',
+        'route' => 'dashboard.categories.index',
+        'title' => 'Orders',
+        'active' => 'dashboard.orders.*',
+        'ability' => 'orders.view',
     ],
-    ['icon'=>'far fa-shield nav-icon',
-     'route'=> 'dashboard.roles.index',
-     'title'=> 'Roles' ,
-     'active'=>'dashboard.orders.*',
-     'ability'=>'roles.view',
-
-   
-    ]
-
+    [
+        'icon' => 'fas fa-shield nav-icon',
+        'route' => 'dashboard.roles.index',
+        'title' => 'Roles',
+        'active' => 'dashboard.roles.*',
+        'ability' => 'roles.view',
+    ],
+    [
+        'icon' => 'fas fa-users nav-icon',
+        'route' => 'dashboard.users.index',
+        'title' => 'Users',
+        'active' => 'dashboard.users.*',
+        'ability' => 'users.view',
+    ],
+    [
+        'icon' => 'fas fa-users nav-icon',
+        'route' => 'dashboard.admins.index',
+        'title' => 'Admins',
+        'active' => 'dashboard.admins.*',
+        //'ability' => 'admins.view',
+    ],
 ];
