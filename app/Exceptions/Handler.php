06fcd8778c55108d04bdc1c5cb818ad1c9b8e660
mariam <mariamemampp00@gmail.com>
2025-01-07 19:59:50 +0200

    edit the handler and the properties of it

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,8 +25,34 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->reportable(function (QueryException $e) {
+           if($e->getCode()=== '23000'){
+            Log::channel('sql')->warning($e->getMessage());
+            return false;
+
+           }
+            QueryException::class;
+        });
+
+        $this->renderable(function (QueryException $e , $request) {
+           if($e->getCode() == 23000){
+             $message = ' Foreign Key constraint failed';
+           } else {
+                $message = $e->getMessage();
+           }
+
+           if($request->expectsJson()){
+            return response()->json([
+                'message'=>$message,
+            ],400);
+           }
+            return redirect()
+            ->back()
+            ->withInput()->withErrors([
+                'message' => $e->getMessage(),
+            ])
+            ->with('info', $message);
         });
     }
+
 }
