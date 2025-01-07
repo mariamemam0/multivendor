4606a1a0a35b010247b59e45df1b7647a6e54013
mariam <mariamemampp00@gmail.com>
2025-01-07 20:00:53 +0200

    change the default channel of the logs and make our custom log

--- config/logging.php
+++ config/logging.php
@@ -65,6 +65,14 @@ return [
             'replace_placeholders' => true,
         ],
 
+        'sql' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/sql.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'replace_placeholders' => true,
+        ],
+
+
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
