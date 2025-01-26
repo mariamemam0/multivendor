8eb1bbe687c3453c44b98653f9a52a9470452e7e
mariam <mariamemampp00@gmail.com>
2025-01-26 14:52:40 +0200

    relation between order and delivery

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -50,6 +50,11 @@ class Order extends Model
         return $this->hasOne(OrderAddress::class,'order_id','id')
         ->where('type' , '=','shipping');
     }
+
+    public function delivery()
+    {
+        return $this->hasOne(Delivery::class);
+    }
     protected static function booted()
     {
         static::creating(function(Order $order){
@@ -65,4 +70,5 @@ class Order extends Model
           }
           return $year . '0001';
     }
+    
 }
